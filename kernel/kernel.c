d48db579f001c0fec3967dc8548e23cbe2030f87
Lachlan Jowett <lachlan@jowett.net.au>
2023-07-21 16:15:08 +1000

    added some superficial stuff

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -2,6 +2,9 @@
 
 #include "system/gdt.h"
 
+const char* KERNEL_VERSION = "v0.1.0";
+const char* USER = "Lochyj";
+
 void kmain() {
 	initialise_console();
 
@@ -10,6 +13,13 @@ void kmain() {
 	
 
 
-	printf("Hello, kernel World!\n");
+	printf(" ____  _ _       _     ____   _____\n");
+    printf("|  _ \\| (_)     | |   / __ \\ / ____|\n");
+    printf("| |_) | |_ _ __ | | _| |  | | (___\n");
+    printf("|  _ <| | | '_ \\| |/ / |  | |\\___ \\ \n");
+    printf("| |_) | | | | | |   <| |__| |____) |\n");
+    printf("|____/|_|_|_| |_|_|\\_\\\\____/|_____/\n");
+    printf("Kernel version %s; User: %s\n", KERNEL_VERSION, USER);
+    printf("console@%s> ", USER);
 
 }
